ac0a417b9783d0d6876eeccac0796894062f947b
Mark <mark.b.hall.1117@gmail.com>
2026-02-19 09:47:43 -0700

    More fixes

---
+++ tools/SirThaddeus.Harness/Artifacts/ArtifactPaths.cs
@@ -0,0 +1,13 @@
+namespace SirThaddeus.Harness.Artifacts;
+
+public sealed record ArtifactPaths
+{
+    public required string RootDirectory { get; init; }
+    public required string InputJsonPath { get; init; }
+    public required string StepsJsonlPath { get; init; }
+    public required string FinalTextPath { get; init; }
+    public required string ScoreJsonPath { get; init; }
+    public required string DiffMarkdownPath { get; init; }
+    public required string JudgePacketPath { get; init; }
+    public required string JudgeResultPath { get; init; }
+}
